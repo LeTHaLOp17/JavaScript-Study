d1a90ab0503ba976cb64f3c4819ed9cd910744a6
LeTHaLOp17 <aksingh6224@gmail.com>
2024-12-02 10:48:01 +0530

    flat

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,5 +10,9 @@ const dc_heroes = ["Superman", "flash", "Batman"];
 //concat will add all the elemens as an signle array and give the output
 
 
-const all_heroes = [...marvel_heroes, ...dc_heroes];
-console.log(all_heroes);   //Same as concat but in this method(spread method) we can add more than two array
+// const all_heroes = [...marvel_heroes, ...dc_heroes];
+// console.log(all_heroes);   //Same as concat but in this method(spread method) we can add more than two array
+
+const array = [1, 2, 3, [4, 5, 6], 7, [6, 8, [4,7]]];
+const real_array = array.flat(Infinity);
+console.log(real_array);
