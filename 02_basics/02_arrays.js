869c811ec58a4356e566d245b181d90af2b9fa62
LeTHaLOp17 <aksingh6224@gmail.com>
2024-12-02 10:37:49 +0530

    concat method

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,7 +1,10 @@
-const marvel_heroes = ['Thor', 'IronMan', 'Captain America', 'Hawkeye'
-];
-const dc_heroes = ['Superman', 'flash', 'Batman'];
+const marvel_heroes = ["Thor", "IronMan", "Captain America"];
+const dc_heroes = ["Superman", "flash", "Batman"];
 
-marvel_heroes.push(dc_heroes);
-console.log(marvel_heroes); //['Thor', 'IronMan', 'Captain America', 'Hawkeye', ['Superman', 'flash', 'Batman']]
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes); //['Thor', 'IronMan', 'Captain America', 'Hawkeye', ['Superman', 'flash', 'Batman']]
 //push will add all the elemens as an array and make it a 2d array on last element
+
+const all_heroes = marvel_heroes.concat(dc_heroes)
+console.log(all_heroes); //['Thor', 'IronMan', 'Captain America', 'Hawkeye','Superman', 'flash', 'Batman']
+//concat will add all the elemens as an signle array and give the output
