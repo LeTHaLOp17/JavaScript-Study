0d9a689caaf780992b08146d6e7b8347bbcbff50
LeTHaLOp17 <aksingh6224@gmail.com>
2024-12-02 10:43:19 +0530

    Spread method

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -5,6 +5,10 @@ const dc_heroes = ["Superman", "flash", "Batman"];
 // console.log(marvel_heroes); //['Thor', 'IronMan', 'Captain America', 'Hawkeye', ['Superman', 'flash', 'Batman']]
 //push will add all the elemens as an array and make it a 2d array on last element
 
-const all_heroes = marvel_heroes.concat(dc_heroes)
-console.log(all_heroes); //['Thor', 'IronMan', 'Captain America', 'Hawkeye','Superman', 'flash', 'Batman']
+// const all_heroes = marvel_heroes.concat(dc_heroes)
+// console.log(all_heroes); //['Thor', 'IronMan', 'Captain America', 'Hawkeye','Superman', 'flash', 'Batman']
 //concat will add all the elemens as an signle array and give the output
+
+
+const all_heroes = [...marvel_heroes, ...dc_heroes];
+console.log(all_heroes);   //Same as concat but in this method(spread method) we can add more than two array
